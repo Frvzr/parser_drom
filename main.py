bffbd642ffa3bb57b23e3f728234b53b5b912adc
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-07-11 23:52:12 +0500

    parse link and car_name

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ print(page.status_code)
 
 soup = bs(page.text, 'html.parser')
 
-cars = soup.find_all('div', class_ = 'css-1nvf6xk eaczv700')
-for car in cars:
-    link = 
-    name = 
-    description =
+cars = soup.find('div', class_ = 'css-1nvf6xk eaczv700')
+#print(cars)
+
+for item in cars:
+    a = item.find_all('a', class_ = 'css-5l099z ewrty961')
+    car_name = item.find_all('span', {'data-ftid': 'bull_title'})
     
 
 if __name__ == '__main__':
