4a7297c483faf14b852eebb945d50b46e3557c01
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-07-12 23:22:47 +0500

    get data about car, create dict with data

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+# -*- coding: utf-8 -*-
+
 from config import URL
 import requests
 from bs4 import BeautifulSoup as bs
+import re
 
 page = requests.get(URL)
 print(page.status_code)
 
 soup = bs(page.text, 'html.parser')
 
-cars = soup.find('div', class_ = 'css-1nvf6xk eaczv700')
-#print(cars)
+cars_dict = {}
+cars = soup.find_all('a', class_ = 'css-5l099z ewrty961')
 
-for item in cars:
-    a = item.find_all('a', class_ = 'css-5l099z ewrty961')
-    car_name = item.find_all('span', {'data-ftid': 'bull_title'})
-    
+for car in cars:
+    link = car.get('href')
+    id = re.findall('\d{8}', str(link))
+    car_name = car.find('span', {'data-ftid': 'bull_title'}).text.split(',')
+    desc = car.find('span', {'data-ftid': 'bull_description-item'}).contents
+    price = car.find('span', {'data-ftid': 'bull_price'}).text
+    price_ = re.sub(r"\s+", "", price)
+    desc = car.find_all('span', class_ = 'css-1l9tp44 e162wx9x0')
+    description = []
+    for i in desc:
+        desc.append(i.text.strip(','))
+
+    cars_dict.setdefault(*id, [*car_name, *description, link, price_])
+
+print(cars_dict)
+
 
 if __name__ == '__main__':
     pass
