a183012592c5f9a10030c3c6cd8644b8babd32fb
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-07-14 23:25:58 +0500

    add format file function

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 import openpyxl
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.styles import Alignment
 
 def get_request(url):
     page = requests.get(url)
@@ -29,11 +31,11 @@ def collect_data(cars):
         description = []
         for i in desc:
             description.append(i.text.strip(','))
-
         cars_dict.setdefault(*id, [*car_name, *description, link, price_])
-
-    #print(cars_dict)
     return create_file(cars_dict)
+
+def get_qty_cars():
+    pass
 
 def create_file(cars_dict):
     try:
@@ -46,11 +48,25 @@ def create_file(cars_dict):
     for row, (key, values) in enumerate(cars_dict.items(), start=2):
         ws[f'A{row}'] = key
         ws[f'B{row}'] = values[0]
+    return format_file(wb, ws)
+
+def format_file(wb, ws):
+    #qty_cars = get_qty_cars()
+    #tab = Table(displayName="Table1", ref="A1:J{}".format(qty_cars))
+    tab = Table(displayName="Table1", ref="A1:J50")
+    style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False, showLastColumn=False, showRowStripes=True, showColumnStripes=False)
+    tab.tableStyleInfo = style
+    ws.add_table(tab)
+    col_dimensions = {'A': 15, 'B': 20, 'C': 12, 'D': 12, 'E': 15, 'F': 15, 'G': 15, 'H': 15, 'I': 30, 'J': 15}
+    for i in range(50):
+        ws.row_dimensions[i].height = 20
+    for key, value in col_dimensions.items():
+        ws.column_dimensions[key].width = value  
     return save_excel(wb)
 
 def save_excel(wb):
     try:
-        f_name = f'cars.xlsx'       
+        f_name = f'C:\\Users\\user\\Desktop\\cars.xlsx'       
         wb.save(f_name)
         message = f'данные записаны в файл \n'
     except:
